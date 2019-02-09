18c4417037807b97b7e15c8a9458899bc6319b6f
Suncove <39926147+Suncove@users.noreply.github.com>
2019-02-08 17:58:45 -1000

    Update GalaxyClassifier.py

--- GalaxyClassifier.py
+++ GalaxyClassifier.py
@@ -1,10 +1,6 @@
 #! /usr/bin/env python3
 
-<<<<<<< HEAD
 #Copyright 2019 Kyle Steckler
-=======
-# Copyright 2019 Kyle Steckler
->>>>>>> 76cf2aac68482463da22774549283b6f763d1fdc
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this
 # software and associated documentation files (the "Software"), to deal in the Software 
